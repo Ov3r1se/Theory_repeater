600a8def4459d9b4fe49cc69389c9ed09f7fe122
De_K1r0 <75795676+Ov3r1se@users.noreply.github.com>
2023-01-31 23:05:03 +0300

    v_1
    Самая первая версия
    (с кривым отображением результатов)

---
+++ Theory_reminder/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Theory_reminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(720, 591);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 68);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(465, 518);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(693, 41);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(1365, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(128, 87);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1393, 644);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 48);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(274, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1011, 87);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(274, 129);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(1011, 361);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.WordWrap = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(584, 605);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 40);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(907, 605);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 40);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1505, 704);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Theory_reminder/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Theory_reminder
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> CONSTANT_list_Answer_Question = new List<string>();
+
+        List<string> UPDATING_list_Question = new List<string>();
+
+        List<string> list_Success_answers = new List<string>();
+        List<string> list_Failure_answers = new List<string>();
+
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Maximized;
+
+        }
+
+ 
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = System.IO.Directory.GetCurrentDirectory();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                Console.WriteLine("123");
+
+                string pathWithTopics = folderBrowserDialog1.SelectedPath;
+
+                string pathToCurrent_txt_file = "";
+
+                string[] sections_topics = Directory.GetFiles(pathWithTopics, "*.txt");
+
+                if (sections_topics.Length == 0) MessageBox.Show("Темы в данном каталоге не обнаружены");
+
+                //string nameVideoFile = "";
+                string nameTextFile = "";
+
+                for (int i = 0; i < sections_topics.Length; i++)
+                {
+                    //nameVideoFile = Path.GetFileName(sections_of_the_selected_topic[i]).ToString();
+                    nameTextFile = Path.GetFileName(sections_topics[i]).ToString();
+
+                    //nameTextFile = "v_" + nameVideoFile.Replace("mp4", "txt");
+
+                    pathToCurrent_txt_file = sections_topics[i];
+
+                    bool txtFileExists = File.Exists(pathToCurrent_txt_file);
+
+                    if (txtFileExists)
+                    {
+                        //pathTo_txt_file = Environment.CurrentDirectory + "\\text_files\\" + "v_" + listBox1.SelectedItem.ToString().Replace("\t      *", "") + ".txt";
+
+                        string[] allLinesTxtFile = File.ReadAllLines(pathToCurrent_txt_file);
+
+                        string fullStringFromFile = File.ReadAllText(pathToCurrent_txt_file);
+
+                        string[] individual_questions = fullStringFromFile.Split('~');
+
+                        string answer = "";
+                        string question = "";
+
+                        //Dictionary<int, string> dict_Answer_Question = new Dictionary<int, string>(); 
+                        
+
+                        for (int t = 0; t < individual_questions.Length; t++)
+                        {
+                            question = individual_questions[t].Split('►')[0]; Console.WriteLine("question = " + question);
+                            answer = individual_questions[t].Split('►')[1];   Console.WriteLine("answer = " + answer);
+
+                            if(!CONSTANT_list_Answer_Question.Contains(question + "►" + answer)) CONSTANT_list_Answer_Question.Add(question + "►" + answer);
+
+                            if (!UPDATING_list_Question.Contains(question + "►" + answer)) UPDATING_list_Question.Add(question + "►" + answer);
+
+                            
+                            //listBox1.Items.Add(question);
+
+
+                        }
+                        textBox2.Text = "0/" + individual_questions.Length;
+
+                        
+                        int rnd = random.Next(0, CONSTANT_list_Answer_Question.Count);
+                        textBox3.Text = UPDATING_list_Question.ElementAt(rnd).ToString().Split('►')[0];
+                        textBox4.Text = UPDATING_list_Question.ElementAt(rnd).ToString().Split('►')[1];
+
+                        //MessageBox.Show("Загруженных вопросов : " + individual_questions.Length);
+
+                    }
+                    //else
+
+                        //listBox1.Items.Add(Path.GetFileName(sections_of_the_selected_topic[i]).Replace(".mp4", ""));
+                }
+
+            }
+
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+            {
+
+   
+                if(UPDATING_list_Question.Count != 0)
+                {
+                    if (textBox4.Text.Contains(textBox1.Text))
+                    {
+                        //MessageBox.Show("Верно");
+                        textBox1.ForeColor = Color.Green;
+                        if (!list_Success_answers.Contains(textBox3.Text)) list_Success_answers.Add(textBox3.Text);
+
+                        if (UPDATING_list_Question.Count != 0) UPDATING_list_Question.Remove(textBox3.Text + '►' + textBox4.Text);
+
+                    }
+                    else
+                    {
+                        //MessageBox.Show("Не верно");
+                        textBox1.ForeColor = Color.Red;
+
+                        if (UPDATING_list_Question.Count != 0) UPDATING_list_Question.Remove(textBox3.Text + '►' + textBox4.Text);
+                        if (!list_Failure_answers.Contains(textBox3.Text)) list_Failure_answers.Add(textBox3.Text);
+                    }
+
+                }
+
+
+
+
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            if(UPDATING_list_Question.Count != 0)
+            {
+                int rnd = random.Next(0, UPDATING_list_Question.Count);
+                textBox3.Text = UPDATING_list_Question.ElementAt(rnd).ToString().Split('►')[0];
+                textBox4.Text = UPDATING_list_Question.ElementAt(rnd).ToString().Split('►')[1];
+                textBox1.ForeColor = Color.Black;
+            }
+            else if (UPDATING_list_Question.Count == 0 && list_Success_answers.Count != 0 || list_Failure_answers.Count != 0)
+            {
+                MessageBox.Show("Все вопросы пройдены");
+
+
+                string SuccessResult = "Верно отвечено на : " + '\r' + '\n';;
+                for (int i = 0; i < list_Success_answers.Count; i++)
+                {
+                    SuccessResult += list_Success_answers[i] + '\r' + '\n';
+                }
+
+                //textBox4.Text = result;
+
+
+                //string FailureResult = "Нужно поизучать :" + '\r' + '\n';
+
+                SuccessResult += "Нужно поизучать :" + '\r' + '\n'; ;
+
+
+                for (int i = 0; i < list_Failure_answers.Count; i++)
+                {
+                    SuccessResult += list_Failure_answers[i] + '\r' + '\n';
+                }
+
+                textBox4.Text = SuccessResult;
+
+
+
+            }
+
+        }
+    }
+}
